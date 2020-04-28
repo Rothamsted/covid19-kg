6919d952fc7a6cb6c0325506cc0c2d9ede65e7c4
Joe <47788638+josephhearnshaw@users.noreply.github.com>
2020-04-28 16:51:26 +0100

    updated to use all data + faster
    Currently chunks (whole db too) DrugBank into 4 files. Non automated, add automation of chunking of DF's in future. Significantly speeds it up.
    
    Added additional checks for results that return null due to missing JSON field; they still contain a parent ChEMBL ID.

--- ETL/db-2-chembl.py
+++ ETL/db-2-chembl.py
@@ -1,7 +1,7 @@
 from chembl_webresource_client.new_client import new_client
 import pandas as pd
 import progressbar as pb
-import argparse, time
+import argparse, time, os
 
 def timeFormat(secs):
     """ Formats Seconds to relevant time interval"""
@@ -17,43 +17,79 @@ def timeFormat(secs):
     ("{0} second{1} ".format(seconds, "s" if seconds!=1 else "") if seconds else "")
     return time
 
-def mapData(db_dir, base_dir, release_ver):
-    db_dir = f"{db_dir}/DrugBank.csv"
-    drugbank_df = pd.read_csv(db_dir)
-    drugbank_df = drugbank_df.drop_duplicates()
-    drug_names = drugbank_df['Name'].tolist()
 
-    chembl_id_list,drugbank_list_updated  = [], []
-    drug_names = list(dict.fromkeys(drug_names))
-    chunks = (len(drug_names) - 1) // 50 + 1 # Chunk into 50 to be handled by EBI as fast as possible.
+def searchChEMBL(chunks, drugname,chembl_id_list, drugbank_list_updated):
 
     widgets = ['Test: ', pb.Percentage(), ' ', pb.Bar(marker='0',left='[',right=']'), ' ', pb.ETA(), ' ', pb.FileTransferSpeed()]
     pbar = pb.ProgressBar(widgets=widgets, maxval=chunks)
-    cv_start_time = time()
     pbar.start()
-
+    j = None
+    count = 0
     for i in range(chunks):
         batch = drug_names[i*50:(i+1)*50]
         for drug in batch:
             try:
-                chembl_id_list.append(new_client.molecule.search(drug)[0]['molecule_chembl_id'])
-                drugbank_list_updated.append(drug)
-                print(drug)
+                if drug != j:
+                    chembl_id_list.append(new_client.molecule.search(drug)[0]['molecule_hierarchy']['parent_chembl_id'])
+                    drugbank_list_updated.append(drug)
+                    j = drug
+                    count+=1
             except:
-                pass
-        time.sleep(0.5) # limit throttling on EBI network, if present.
+                try:
+                    if new_client.molecule.search(drug)[0]['molecule_chembl_id'] != '[]':
+                        chembl_id_list.append(new_client.molecule.search(drug)[0]['molecule_chembl_id'])
+                        drugbank_list_updated.append(drug)
+                except:
+                    pass
+        time.sleep(0.1)
         pbar.update(i)
 
+
+def mapData(db_dir, base_dir, release_ver):
+    db_dir = f"{db_dir}/DrugBank.csv"
+    drugbank_df = pd.read_csv(db_dir)
+    drugbank_df = drugbank_df.drop_duplicates()
+    # Functions
+    loc_names = lambda x,y: drugbank_df.iloc[x:y, :]['Name'].tolist()
+    chunk_it = lambda x:  (len(x) - 1) // 50 + 1
+
+    drug_names_1, drug_names_2 = loc_names(0,3391), loc_names(3391,6782)
+    drug_names_3, drug_names_4 = loc_names(6782,10172), loc_names(10172, 13563)
+
+    chembl_id_list_1,drugbank_list_updated_1  = [], []
+    chembl_id_list_2,drugbank_list_updated_2  = [], []
+    chembl_id_list_3,drugbank_list_updated_3  = [], []
+    chembl_id_list_4,drugbank_list_updated_4  = [], []
+
+    chunks_1, chunks_2 = chunk_it(drug_names_1), chunk_it(drug_names_2)
+    chunks_3, chunks_4 = chunk_it(drug_names_3), chunk_it(drug_names_4)
+    cv_start_time = time.time()
+
+    searchChEMBL(chunks_1, drug_names_1,chembl_id_list_1, drugbank_list_updated_1)
+    print("Finished batch 1 of 4\n")
+    searchChEMBL(chunks_2, drug_names_2,chembl_id_list_2, drugbank_list_updated_2)
+    print("Finished batch 2 of 4\n")
+    searchChEMBL(chunks_3, drug_names_3,chembl_id_list_3, drugbank_list_updated_3)
+    print("Finished batch 3 of 4\n")
+    searchChEMBL(chunks_4, drug_names_4,chembl_id_list_4, drugbank_list_updated_4)
+    print("Finished batch 4 of 4\n")
+
+    chembl_id_list_ = chembl_id_list_1 + chembl_id_list_2 + chembl_id_list_3 + chembl_id_list_4
+    drugbank_list_updated_ = drugbank_list_updated_1 + drugbank_list_updated_2 + drugbank_list_updated_3 + drugbank_list_updated_4
+
     chembl_to_db_json =  {
-                            'Name': drugbank_list_updated,
-                            'ChEMBL ID': chembl_id_list
+                            'Name': drugbank_list_updated_,
+                            'ChEMBL ID': chembl_id_list_
                          }
 
     chembl_to_db_pd = pd.DataFrame(chembl_to_db_json, columns=['Name', 'ChEMBL ID'])
     chembl_to_db_pd_updated = chembl_to_db_pd.merge(drugbank_df, on=['Name', 'Name'], how='inner')
-    chembl_to_db_pd_updated = chembl_to_db_pd_updated[['DrugBank', 'ChEMBL ID', 'Name']]
+    chembl_to_db_pd_updated = chembl_to_db_pd_updated[['DrugBank ID', 'ChEMBL ID', 'Name']]
+    chembl_to_db_pd_updated.columns = ['DrugBank ID', 'ChEMBL ID', 'Drug Name']
+    chembl_to_db_pd_updated = chembl_to_db_pd_updated.drop_duplicates()
+
     chembl_to_db_pd_updated.to_csv(f"{base_dir}{release_ver}/humanKnet/organisms/homo_sapiens/drug/db_chembl_mapping.tsv", sep="\t", index=None, header=True)
-    print(f"\nData written out to {base_dir}{release_ver}/humanKnet/organisms/homo_sapiens/drug/db_chembl_mapping.tsv\nTask completed in {timeFormat(time()-cv_start_time)}\n")
+    print(f"\nData written out to {base_dir}{release_ver}/humanKnet/organisms/homo_sapiens/drug/db_chembl_mapping.tsv\nTask completed in {timeFormat(time.time()-cv_start_time)}\n")
 
 parser = argparse.ArgumentParser(
     description="This script will convert DrugBank ID's to ChEMBL ID's\n")
